eb01f768579bc9d66504e3475e5e671f2087e63e
saurabhsurve04 <saurabhsurve04@gmail.com>
2026-03-19 23:46:02 +0530

    Week 6 complete - Page Object Model with BasePage, LoginPage, SecurePage

---
+++ qa-automation-suite/qa-automation-suite/src/test/java/com/saurabh/pages/SecurePage.java
@@ -0,0 +1,28 @@
+package com.saurabh.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class SecurePage extends BasePage {
+
+    private By successMessage = By.tagName("b");
+    private By logoutButton = By.xpath("//i[contains(text(),'Logout')]");
+
+    public SecurePage(WebDriver driver) {
+        super(driver);
+    }
+
+    public String getSuccessMessage(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(successMessage));
+        return driver.findElement(successMessage).getText();
+    }
+
+    public void clickLogoutButton(){
+        driver.findElement(logoutButton).click();
+    }
+
+    public String getCurrentUrl(){
+        return driver.getCurrentUrl();
+    }
+}
